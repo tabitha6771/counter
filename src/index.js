e80ff6ccdbd3c0cfd7ee79bc3aa26df461183ea9
tabitha6771 <67273824+tabitha6771@users.noreply.github.com>
2020-07-28 16:04:40 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+
+const styles = {
+  fontFamily: "Arial",
+  textAlign: "center"
+};
+
+render(<App />, document.getElementById("root"));
